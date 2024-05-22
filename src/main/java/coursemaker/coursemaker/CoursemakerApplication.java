fcd724ba588bc2a4a6e9bf2ff1f8838c480c1f9d
isaiah IM <isaiahim0214@gmail.com>
2024-05-22 17:43:58 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/coursemaker/coursemaker/CoursemakerApplication.java
@@ -0,0 +1,13 @@
+package coursemaker.coursemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursemakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursemakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coursemaker/coursemaker/CoursemakerApplicationTests.java
@@ -0,0 +1,13 @@
+package coursemaker.coursemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
