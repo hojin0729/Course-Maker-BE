9fc06a89a8a29b85a2a94e25063f3456e81650c5
anjunsu <junsu6752@gmail.com>
2024-05-24 22:41:28 +0900

    Feat: course 도메인 서비스, 리포지토리 통일

--- src/main/java/coursemaker/coursemaker/domain/course/dto/TravelCourseResponse.java
+++ src/main/java/coursemaker/coursemaker/domain/course/dto/TravelCourseResponse.java
@@ -9,14 +9,20 @@ import java.time.LocalDateTime;
 public class TravelCourseResponse {
 
     private final String title;
-    private final String description;
+    private final String content;
     private final LocalDateTime createdAt;
+    private final int duration;
+    private final int travelerCount;
+    private final int travelType;
 //    private final Member member;
 
     public TravelCourseResponse(TravelCourse travelCourse) {
         this.title = travelCourse.getTitle();
-        this.description = travelCourse.getDescription();
+        this.content = travelCourse.getContent();
         this.createdAt = travelCourse.getCreatedAt();
+        this.duration = travelCourse.getDuration();
+        this.travelerCount = travelCourse.getTravelerCount();
+        this.travelType = travelCourse.getTravelType();
 //        this.member = travelCourse.getMember();
     }
 }
