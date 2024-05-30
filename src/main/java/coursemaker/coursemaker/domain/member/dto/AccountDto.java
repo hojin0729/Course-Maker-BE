d773485effc32e8785a5f981f7531ed110c63163
YachaTree <dnftksghkfkd@icloud.com>
2024-05-30 10:00:57 +0900

    fix: Member Entity field 변경

--- src/main/java/coursemaker/coursemaker/domain/member/dto/AccountDto.java
+++ src/main/java/coursemaker/coursemaker/domain/member/dto/AccountDto.java
@@ -1,5 +1,7 @@
 package coursemaker.coursemaker.domain.member.dto;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,8 +13,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AccountDto {
     private String id;
-    private String username;
-    private String password;
-    private int age;
-    private String roles;
+    @NotBlank(message = "아이디를 입력하세요")
+    private String username; //회원 id
+
+    @NotBlank(message = "이름을 입력하세요")
+    private String name; //회원 이름
+
+    @NotBlank(message = "이메일을 입력하세요")
+    private String email; // 회원 이메일
+    private String nickname; //회원 닉네임
+    private String phoneNumber; //회원 전화번호
+    private String password; //회원 비밀번호
+    private String roles; //회원 등급
 }
