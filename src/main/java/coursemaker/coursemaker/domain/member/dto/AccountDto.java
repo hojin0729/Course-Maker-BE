67e233cd06bd5a692c38d7472192d534088510a2
YachaTree <dnftksghkfkd@icloud.com>
2024-05-30 11:54:02 +0900

    feat: member도메인 swagger&api 작성 및 gradle 설정추가(email)

--- src/main/java/coursemaker/coursemaker/domain/member/dto/AccountDto.java
+++ src/main/java/coursemaker/coursemaker/domain/member/dto/AccountDto.java
@@ -13,13 +13,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AccountDto {
     private String id;
-    @NotBlank(message = "아이디를 입력하세요")
     private String username; //회원 id
-
-    @NotBlank(message = "이름을 입력하세요")
     private String name; //회원 이름
-
-    @NotBlank(message = "이메일을 입력하세요")
     private String email; // 회원 이메일
     private String nickname; //회원 닉네임
     private String phoneNumber; //회원 전화번호
--- src/main/java/coursemaker/coursemaker/domain/member/entity/Member.java
+++ src/main/java/coursemaker/coursemaker/domain/member/entity/Member.java
@@ -16,19 +16,26 @@ public class Member implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column
-    private String username; //회원 id
-    @Column
-    private String name; //회원 이름
-    @Column
+
+    @Column(nullable = false, unique = true)
+    private String username; // 회원 id
+
+    @Column(nullable = false)
+    private String name; // 회원 이름
+
+    @Column(nullable = false, unique = true)
     private String email; // 회원 이메일
-    @Column
-    private String nickname; //회원 닉네임
-    @Column
-    private String phoneNumber; //회원 전화번호
-    @Column
-    private String password; //회원 비밀번호
-    @Column
-    private String roles; //회원 등급
+
+    @Column(nullable = false)
+    private String nickname; // 회원 닉네임
+
+    @Column(nullable = false, length = 15)
+    private String phoneNumber; // 회원 전화번호
+
+    @Column(nullable = false)
+    private String password; // 회원 비밀번호
+
+    @Column(nullable = false)
+    private String roles; // 회원 등급
 
 }
