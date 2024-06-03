17b111c6efea337b822b9026477620b87c184cd1
isaiah IM <isaiahim0214@gmail.com>
2024-06-04 02:09:31 +0900

    Refactor: 태그 도메인 삭제쿼리 최적화

--- src/main/java/coursemaker/coursemaker/exception/GlobalExceptionHandler.java
+++ src/main/java/coursemaker/coursemaker/exception/GlobalExceptionHandler.java
@@ -4,4 +4,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    // root exception을 잡을때
+    // 그 외의 exception을 잡을때
 }
