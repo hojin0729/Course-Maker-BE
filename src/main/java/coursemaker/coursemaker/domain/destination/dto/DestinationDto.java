29244756ed3df2fd64d111e6f965a3890bd1c96a
hojin0729 <0918syj@gmail.com>
2024-06-04 15:33:10 +0900

    Refactor: Swagger 리팩토링

--- src/main/java/coursemaker/coursemaker/domain/destination/dto/DestinationDto.java
+++ src/main/java/coursemaker/coursemaker/domain/destination/dto/DestinationDto.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Data
 public class DestinationDto {
     private Long id;
-    private String username; // 유저 이름
+    private String nickname; // 유저 이름
     private String name; // 여행지 이름
     private List<TagResponseDto> tags; // 태그 리스트
     private String location; // 위치 이름
@@ -25,7 +25,7 @@ public class DestinationDto {
     private LocalDateTime updatedAt; // 수정 날짜
 
     // DestinationDto를 Destination 엔티티로 변환하는 메서드
-    public static Destination toEntity(DestinationDto dto) {
+    public static Destination toEntity(RequestDto dto) {
         Destination destination = new Destination();
         destination.setName(dto.getName());
         destination.setPictureLink(dto.getPictureLink());
@@ -40,7 +40,7 @@ public class DestinationDto {
     public static DestinationDto toDto(Destination destination, List<TagResponseDto> tagDtos) {
         DestinationDto dto = new DestinationDto();
         dto.setId(destination.getId());
-//        dto.setUsername(destination.getMember().getUsername()); // 누가 만들었는지 설정
+//        dto.setNickname(destination.getMember().getNickname()); // 누가 만들었는지 설정
         dto.setName(destination.getName());
         dto.setPictureLink(destination.getPictureLink());
         dto.setContent(destination.getContent());
