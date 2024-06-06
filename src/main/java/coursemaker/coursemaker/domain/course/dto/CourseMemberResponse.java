c880be1b7e2a91f7cbe2a8d73711d9fa4975fc0e
anjunsu <junsu6752@gmail.com>
2024-06-06 15:10:15 +0900

    Feat: Course 도메인 dto 스웨거 작성

--- src/main/java/coursemaker/coursemaker/domain/course/dto/CourseMemberResponse.java
+++ src/main/java/coursemaker/coursemaker/domain/course/dto/CourseMemberResponse.java
@@ -1,12 +1,16 @@
 package coursemaker.coursemaker.domain.course.dto;
 
 import coursemaker.coursemaker.domain.member.entity.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+@Schema(description = "멤버에서 코스로 닉네임 가져오는 DTO")
 @Data
 public class CourseMemberResponse {
+
+    @Schema(description = "코스를 만든 유저의 닉네임", example = "nickname1")
     private String nickname;
 
     public CourseMemberResponse(Member member) {
--- src/main/java/coursemaker/coursemaker/domain/course/dto/TravelCourseResponse.java
+++ src/main/java/coursemaker/coursemaker/domain/course/dto/TravelCourseResponse.java
@@ -2,6 +2,7 @@ package coursemaker.coursemaker.domain.course.dto;
 
 import coursemaker.coursemaker.domain.course.entity.TravelCourse;
 import coursemaker.coursemaker.domain.tag.dto.TagResponseDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,19 +10,41 @@ import lombok.Setter;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Schema(description = "여행 코스 응답 DTO")
 @Data
 public class TravelCourseResponse {
 
+    @Schema(description = "코스 id")
     private Long id;
+
+    @Schema(description = "코스 타이틀", example = "Course Title1")
     private final String title;
+
+    @Schema(description = "코스 내용", example = "Course Content1")
     private final String content;
+
+    @Schema(description = "코스 조회 수", example = "777")
     private final Integer views;
+
+    @Schema(description = "여행 기간", example = "3")
     private final Integer duration;
+
+    @Schema(description = "여행 인원", example = "5")
     private final Integer travelerCount;
+
+    @Schema(description = "여행 타입", example = "0")
     private final Integer travelType;
+
+    @Schema(description = "코스 대표 이미지 주소", example = "http://example.com/course1.jpg")
     private final String pictureLink;
+
+    @Schema(description = "코스 여행지 목록")
     private final List<CourseDestinationResponse> courseDestinations;
+
+    @Schema(description = "코스 태그 목록")
     private final List<TagResponseDto> courseTags;
+
+    @Schema(description = "코스 만든 멤버")
     private final CourseMemberResponse member;
 
     public TravelCourseResponse(TravelCourse travelCourse, List<CourseDestinationResponse> courseDestinationResponses) {
