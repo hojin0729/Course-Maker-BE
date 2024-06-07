349e36a9408f68eb4db04054fe6173505d9ec63c
isaiah IM <isaiahim0214@gmail.com>
2024-06-07 16:54:51 +0900

    Feat: base entity deletedAt 생성

--- src/main/java/coursemaker/coursemaker/BaseEntity.java
+++ src/main/java/coursemaker/coursemaker/BaseEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,7 +13,7 @@ import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-@Getter
+@Getter @Setter
 public abstract class BaseEntity {
 
     @CreatedDate
