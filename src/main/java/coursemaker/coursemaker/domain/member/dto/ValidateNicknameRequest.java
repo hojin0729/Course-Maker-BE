afb9dbe58e6990d95d8322ff91ca383261e18231
YachaTree <dnftksghkfkd@icloud.com>
2024-06-09 21:11:06 +0900

    refactor: stubdata 회원 Name수정 및 schema 설명 추가

--- src/main/java/coursemaker/coursemaker/domain/member/dto/ValidateNicknameRequest.java
+++ src/main/java/coursemaker/coursemaker/domain/member/dto/ValidateNicknameRequest.java
@@ -1,8 +1,12 @@
 package coursemaker.coursemaker.domain.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ValidateNicknameRequest {
+    @Schema(description = "닉네임", example = "nickname123")
+    @NotBlank(message = "닉네임을 입력하세요.")
     private String nickname;
 }
