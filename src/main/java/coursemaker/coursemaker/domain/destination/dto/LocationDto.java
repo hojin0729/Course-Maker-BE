fb4855ce4835f6f136d3b56d9bbd89ca873334c0
hojin0729 <0918syj@gmail.com>
2024-06-10 13:26:38 +0900

    Feat: Location Dto 추가

--- src/main/java/coursemaker/coursemaker/domain/destination/dto/LocationDto.java
+++ src/main/java/coursemaker/coursemaker/domain/destination/dto/LocationDto.java
@@ -1,5 +1,6 @@
 package coursemaker.coursemaker.domain.destination.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,10 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LocationDto {
-    private String location; // 위치
+    @Schema(description = "도로명 주소", example = "부산광역시 해운대구 우동")
+    private String address; // 위치
+    @Schema(description = "경도", example = "128.934")
     private BigDecimal longitude; // 경도
+    @Schema(description = "위도", example = "35.169")
     private BigDecimal latitude; // 위도
 }
