c5162578a6c97816c0387a408c9da2802f615bc3
YachaTree <dnftksghkfkd@icloud.com>
2024-06-12 18:05:35 +0900

    refactor: 카카오 회원가입시 닉네임 접미사 추가기능 제거

--- src/main/java/coursemaker/coursemaker/oauth/KakaoService.java
+++ src/main/java/coursemaker/coursemaker/oauth/KakaoService.java
@@ -35,7 +35,7 @@ public class KakaoService {
     public void kakaoSignUp(String kakaoUserId, String kakaoNickname){
         log.info("카카오 회원가입 시작");
         String randomPassword = String.valueOf(UUID.randomUUID()).substring(0,8);
-        String generatedNickname = kakaoNickname + "#" + generateRandomPostfix();
+        String generatedNickname = kakaoNickname;
 
         Member builtUser = Member.addMemberBuilder()
                 .email(kakaoUserId+"@coursemaker.com")
@@ -99,18 +99,5 @@ public class KakaoService {
         log.info("[kakao] 로그아웃 완료");
 
         return LogoutResponse.builder().success(true).build();
-    }
-
-    public String generateRandomPostfix() {
-        int leftLimit = 48; // 숫자 '0'의 ASCII 코드
-        int rightLimit = 122; // 알파벳 'z'의 ASCII 코드
-        int stringLength = 4;
-        Random random = new Random();
-
-        return random.ints(leftLimit, rightLimit + 1) // leftLimit(포함) 부터 rightLimit+1(불포함) 사이의 난수 스트림 생성
-                .filter(i -> (i < 57 || i >= 65) && ( i <= 90 || i >= 97)) // ASCII 테이블에서 숫자, 대문자, 소문자만 사용함
-                .limit(stringLength) // 생성된 난수를 지정된 길이로 잘라냄
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append) // 생성된 난수를 ASCII 테이블에서 대응되는 문자로 변환
-                .toString(); // StringBuilder 객체를 문자열로 변환해 반환
     }
 }
