229edeae358ec9ed3fb61f6dfdbf2dc1be9f2da4
hojin0729 <0918syj@gmail.com>
2024-07-21 21:51:21 +0900

    Feat: event 기본 crud 추가

--- src/main/java/coursemaker/coursemaker/domain/event/repository/EventRepository.java
+++ src/main/java/coursemaker/coursemaker/domain/event/repository/EventRepository.java
@@ -1,9 +1,12 @@
 package coursemaker.coursemaker.domain.event.repository;
 
-import coursemaker.coursemaker.domain.review.entity.CourseReview;
+import coursemaker.coursemaker.domain.event.entity.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends JpaRepository<CourseReview, Long> {
+public interface EventRepository extends JpaRepository<Event, Long> {
+    Page<Event> findAll(Pageable pageable);
 }
