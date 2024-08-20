ba5c21cdfe1788d3b159cb168cb7a747dae8ce49
Derit7042 <junsu6752@gmail.com>
2024-08-21 01:45:15 +0900

    Refactor: 공공데이터 여부를 true, false으로 변경

--- src/main/java/coursemaker/coursemaker/domain/destination/dto/RequestDto.java
+++ src/main/java/coursemaker/coursemaker/domain/destination/dto/RequestDto.java
@@ -45,19 +45,19 @@ public class RequestDto {
     @Schema(description = "평균 평점", example = "4.5", hidden = true)
     private Double averageRating; // 평균 평점
 
-    @Schema(description = "무장애 여행지 여부")
+    @Schema(description = "무장애 여행지 여부", hidden = true)
     private Long disabled;
 
     // tourApi에서 Destination DB로 저장될 때 중복된 데이터 판별 용으로 사용됩니다.
-    @Schema(description = "Tour Api에서 불러온 공공데이터 여행지일 경우 그 여행지에 해당하는 고유 Content ID 값 입니다. tourApi에서 Destination DB로 저장될 때 중복된 데이터 판별 용으로 사용됩니다.")
+    @Schema(description = "Tour Api에서 불러온 공공데이터 여행지일 경우 그 여행지에 해당하는 고유 Content ID 값 입니다. tourApi에서 Destination DB로 저장될 때 중복된 데이터 판별 용으로 사용됩니다.", hidden = true)
     private Long contentId;
 
     // busanApi에서 Destination DB로 저장될 때 중복된 데이터 판별 용으로 사용됩니다.
-    @Schema(description ="busanApi에서 Destination DB로 저장될 때 중복된 데이터 판별 용으로 사용됩니다.")
+    @Schema(description ="busanApi에서 Destination DB로 저장될 때 중복된 데이터 판별 용으로 사용됩니다.", hidden = true)
     private Integer seq;
 
-    @Schema(description = "부산광역시 공공데이터 또는 Tour Api 공공데이터일 경우에 1이라는 값이 할당됩니다.")
-    private Integer apiData;
+    @Schema(description = "부산광역시 공공데이터 또는 Tour Api 공공데이터일 경우에 1이라는 값이 할당됩니다.", hidden = true)
+    private boolean isApiData;
 
 
     // RequestDto를 Destination 엔티티로 변환하는 메서드
@@ -73,7 +73,7 @@ public class RequestDto {
         destination.setAverageRating(this.averageRating);
         destination.setDisabled(this.disabled);
         destination.setContentId(this.contentId);
-        destination.setApiData(this.apiData);
+        destination.setApiData(this.isApiData);
         destination.setSeq(this.seq);
 
         return destination;
