7c41cc40ca13ba5a2003907d2c49c7aae7defeac
Derit7042 <junsu6752@gmail.com>
2024-08-20 22:04:25 +0900

    Feat: 공공데이터에 태그 정보 매핑

--- src/main/java/coursemaker/coursemaker/api/busanApi/dto/BusanApiResponse.java
+++ src/main/java/coursemaker/coursemaker/api/busanApi/dto/BusanApiResponse.java
@@ -1,5 +1,6 @@
 package coursemaker.coursemaker.api.busanApi.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BusanApiResponse {
 
     @JsonProperty("response")
